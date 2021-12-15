1b90d32f395b9ccea3af14b6903164575d548fb4
Luu Luu <luu7755@gmail.com>
2021-12-14 21:18:53 -0600

    corrección de fechas en panel de solicitudes

--- app/src/components/RequestDetails/utils/Scripts/dates.js
+++ app/src/components/RequestDetails/utils/Scripts/dates.js
@@ -1,20 +1,22 @@
 module.exports.dates = (selectedDate, period) => {
 
-    //console.log('Inicia cálculo de fechas')
+    console.log('Inicia cálculo de fechas')
     //Fecha inicial
-    //console.log('Fecha seleccionada: ', selectedDate)
+    console.log('Fecha seleccionada: ', selectedDate)
     //De la fecha seleccionada empieza a correr al siguiente día (le agrega 1 día)
     const startDate = addDays(selectedDate, 1)
-    //console.log(startDate)
+    console.log(startDate)
     //Le da formato para registrarla en la base de datos
     const startDateF = formatDate(startDate)
-    //console.log(startDateF)
+    console.log(startDateF)
+    console.log("días", period)
     //Le suma los días del periodo
-    const finalDate = addDays(startDate, period);
+    const finalDate = addDays(startDate, parseInt(period));
+    console.log(finalDate)
     //Le da formato
     const finalDateF = formatDate(finalDate)
-    //console.log(finalDateF)
-    //console.log('End de fechas')
+    console.log(finalDateF)
+    console.log('End de fechas')
 
     return {
         startDate: startDateF,
